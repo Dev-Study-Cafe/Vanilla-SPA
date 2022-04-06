59fbdbad2319d86f91184eef575e8307d92b933a
Sonwanseo <sonwanseo@gmail.com>
2022-04-06 15:33:05 +0900

    delete: remove no use dependencies

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-
 const express = require("express");
 const path = require("path");
 
-// express 사용
 const app = express();
 
 app.use("/src", express.static(path.resolve(__dirname, "client", "src")));
@@ -11,5 +9,4 @@ app.get("/*", (_, res) => {
     res.sendFile(path.resolve("client", "public/index.html"));
 });
 
-// port 생성 서버 실행
-app.listen(3000, () => console.log("Server running ...."));
+app.listen(3000, () => console.log('\x1b[47m%s\x1b[50', "Server running ..."));
